4cd51dec74276b84413f0f324e51c8994a56bde0
HibaSekkat <hibasekkat136@gmail.com>
2023-05-23 12:25:49 +0100

    test

--- pepper_robot_description/launch/view.launch.py
+++ pepper_robot_description/launch/view.launch.py
@@ -24,6 +24,7 @@ def generate_launch_description() -> LaunchDescription:
     # Get substitution for all arguments
     description_package = LaunchConfiguration("description_package")
     description_filepath = LaunchConfiguration("description_filepath")
+    model = LaunchConfiguration("model")
     name = LaunchConfiguration("name")
     prefix = LaunchConfiguration("prefix")
     gripper = LaunchConfiguration("gripper")
@@ -60,6 +61,9 @@ def generate_launch_description() -> LaunchDescription:
             PathJoinSubstitution(
                 [FindPackageShare(description_package), description_filepath]
             ),
+            " ",
+            "name:=",
+            model,
             " ",
             "name:=",
             name,
@@ -142,6 +146,7 @@ def generate_launch_description() -> LaunchDescription:
             arguments=[
                 "--display-config",
                 rviz_config,
+                model,
                 "--ros-args",
                 "--log-level",
                 log_level,
@@ -177,6 +182,11 @@ def generate_declared_arguments() -> List[DeclareLaunchArgument]:
             "description_filepath",
             default_value=path.join("urdf", "pepper_robot.urdf"),
             description="Path to xacro or URDF description of the robot, relative to share of `description_package`.",
+        ),
+        DeclareLaunchArgument(
+            "model",
+            default_value="pepper_robot",
+            description="Name or filepath of model to load.",
         ),
         # Naming of the robot
         DeclareLaunchArgument(
