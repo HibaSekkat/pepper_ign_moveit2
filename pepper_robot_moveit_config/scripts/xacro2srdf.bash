398b13449618c3238b3aa6b4cb418a996048d6ce
HibaSekkat <hibasekkat136@gmail.com>
2023-05-24 12:27:44 +0100

    test

--- pepper_robot_moveit_config/scripts/xacro2srdf.bash
+++ pepper_robot_moveit_config/scripts/xacro2srdf.bash
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
-# This script converts xacro (SRDF variant) into SRDF for `pepper_robot_description` package
+# This script converts URDF into SRDF for `pepper_robot_description` package
 
 SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
-XACRO_PATH="$(dirname "${SCRIPT_DIR}")/srdf/pepper_robot.xacro"
+URDF_PATH="$(dirname "${SCRIPT_DIR}")/urdf/pepper_robot.urdf"
 SRDF_PATH="$(dirname "${SCRIPT_DIR}")/srdf/pepper_robot.srdf"
-
-# Arguments for xacro
-XACRO_ARGS=(
-    name:=pepper_robot
-    prefix:=pepper_robot_
-)
 
 # Remove old SRDF file
 rm "${SRDF_PATH}" 2>/dev/null
 
-# Process xacro into SRDF
-xacro "${XACRO_PATH}" "${XACRO_ARGS[@]}" -o "${SRDF_PATH}" &&
+# Process URDF into SRDF
+rosrun srdfdom urdf_to_srdf "${URDF_PATH}" "${SRDF_PATH}"
+
 echo "Created new ${SRDF_PATH}"
