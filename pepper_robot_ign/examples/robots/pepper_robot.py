05e4e64e006af4150303b1d03719f703f12a7533
HibaSekkat <hibasekkat136@gmail.com>
2023-06-21 15:36:15 +0100

    test

--- pepper_robot_ign/examples/robots/pepper_robot.py
+++ pepper_robot_ign/examples/robots/pepper_robot.py
@@ -1,25 +1,40 @@
 from typing import List
 
 MOVE_GROUP_ARM: str = "left_arm"
-MOVE_GROUP_GRIPPER: str = "l_gripper"
+MOVE_GROUP_GRIPPER: str = "LHand"
 
 OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [
-    0.2,
-    0.2,
-    0.2,
-    0.2,
-    0.2,
-    0.2,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
 ]
 CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
 ]
-
 
 def joint_names(str="") -> List[str]:
     return [
@@ -31,13 +46,12 @@ def joint_names(str="") -> List[str]:
         "LHand",
     ]
 
-
 def base_link_name(str="") -> str:
-    return "link_base"
+    return "base_link"
 
 
 def end_effector_name(str="") -> str:
-    return "end_effector"
+    return "left_eef"
 
 
 def gripper_joint_names(str="") -> List[str]:
@@ -45,29 +59,15 @@ def gripper_joint_names(str="") -> List[str]:
         "LFinger11",
         "LFinger12",
         "LFinger13",
-        "RFinger11",
-        "RFinger12",
-        "RFinger13",
         "LFinger21",
         "LFinger22",
         "LFinger23",
-        "RFinger21",
-        "RFinger22",
-        "RFinger23",
         "LFinger31",
         "LFinger32",
         "LFinger33",
-        "RFinger31",
-        "RFinger32",
-        "RFinger33",
         "LFinger41",
         "LFinger42",
         "LFinger43",
-        "RFinger41",
-        "RFinger42",
-        "RFinger43",
         "LThumb1",
-        "RThumb1",
         "LThumb2",
-        "RThumb2",
     ]
