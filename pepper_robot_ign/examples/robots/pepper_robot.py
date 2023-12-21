b360b6065c046bf0024505364fc4f11849ab3943
hiba <hibasekkat136@gmail.com>
2023-12-21 14:17:50 +0100

    base_link in srdf

--- pepper_robot_ign/examples/robots/pepper_robot.py
+++ pepper_robot_ign/examples/robots/pepper_robot.py
@@ -4,20 +4,20 @@ MOVE_GROUP_ARM: str = "left_arm"
 MOVE_GROUP_GRIPPER: str = "LHand"
 
 OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
+    0.98,
 ]
 CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [
     0.2,
@@ -39,35 +39,36 @@ CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [
 def joint_names(prefix: str = "") -> List[str]:
     return [
         prefix + "LShoulderPitch",
-        prefix +"LShoulderRoll",
-        prefix +"LElbowYaw",
-        prefix +"LElbowRoll",
-        prefix +"LWristYaw",
-        prefix +"LHand",
+        prefix + "LShoulderRoll",
+        prefix + "LElbowYaw",
+        prefix + "LElbowRoll",
+        prefix + "LWristYaw",
+        prefix + "LGripper",
+        prefix + "LHand",
     ]
 
 def base_link_name(prefix: str = "") -> str:
-    return prefix +"base_link"
+    return prefix + "base_link"
 
 
 def end_effector_name(prefix: str = "") -> str:
-    return prefix +"l_wrist"
+    return prefix + "l_wrist"
 
 
 def gripper_joint_names(prefix: str = "") -> List[str]:
     return [
-        prefix +"LFinger11",
-        prefix +"LFinger12",
-        prefix +"LFinger13",
-        prefix +"LFinger21",
-        prefix +"LFinger22",
-        prefix +"LFinger23",
-        prefix +"LFinger31",
-        prefix +"LFinger32",
-        prefix +"LFinger33",
-        prefix +"LFinger41",
-        prefix +"LFinger42",
-        prefix +"LFinger43",
-        prefix +"LThumb1",
-        prefix +"LThumb2",
+        prefix + "LFinger11",
+        prefix + "LFinger12",
+        prefix + "LFinger13",
+        prefix + "LFinger21",
+        prefix + "LFinger22",
+        prefix + "LFinger23",
+        prefix + "LFinger31",
+        prefix + "LFinger32",
+        prefix + "LFinger33",
+        prefix + "LFinger41",
+        prefix + "LFinger42",
+        prefix + "LFinger43",
+        prefix + "LThumb1",
+        prefix + "LThumb2",
     ]
